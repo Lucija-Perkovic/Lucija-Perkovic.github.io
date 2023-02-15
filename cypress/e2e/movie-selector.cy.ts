d6eb405f98288c3785ecee6e1cb0b80a52195af4
Lucija Perkovic <lucijaperkovic@gmail.com>
2023-02-15 12:42:09 +0100

    Fix failing cypress test

--- cypress/e2e/movie-selector.cy.ts
+++ cypress/e2e/movie-selector.cy.ts
@@ -6,25 +6,24 @@ describe('movie-selector', () => {
     }).as('apiMovieDbSearchMovies')
     cy.intercept({
       method: 'GET',
-      url: 'https://api.themoviedb.org/3/movie*',
+      url: 'https://api.themoviedb.org/3/movie/*',
     }).as('apiMovieDbGetMovieDetails')
     cy.visit('/')
 
   })
   it('should fetch movies when searched and show paginator', () => {
     cy.get('[data-cy="paginator-div"]').should('not.exist')
-    cy.get('input[name="search"]').type('A')
-    cy.wait('@apiMovieDbSearchMovie').then(({ request, response }) => {
+    cy.get('input[name="search"]').type('Last of us')
+    cy.wait('@apiMovieDbSearchMovies').then(({ request, response }) => {
       expect(response.statusCode).to.eq(200)
     })
     cy.get('[data-cy="paginator-div"]').should('exist')
   })
-  it('should fetch movie and open modal when clicking on movie picture', () => {
-    cy.get('input[name="search"]').type('A')
+  it('should fetch movie and open modal when clicking on movie picture on homepage', () => {
+    cy.get('input[name="search"]').type('Avatar')
     cy.get('[data-cy="paginator-div"]').should('exist')
     cy.get('[data-cy="movie-grid"]').should('exist')
-    cy.get('[data-cy="image"]').click()
-    cy.get('[data-cy="modal"]').should('not.be.visible')
+    cy.get('[data-cy="image"]').first().click()
     cy.wait('@apiMovieDbGetMovieDetails')
     cy.get('[data-cy="modal"]').should('be.visible')
   })
